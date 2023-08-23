fd98cf8df1a6db749998de0fcb51abf6314e5e4e
Piotr Balcerzak <piotr.balcerzak@bitbag.pl>
2023-08-23 22:48:12 +0200

    Added team role handling

---
+++ src/Repository/TeamRepository.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Team\Team;
+use App\Entity\User\User;
+use App\Enum\UserRole;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+
+final class TeamRepository extends AbstractRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Team::class);
+    }
+
+    public function indexByUser(User $user): QueryBuilder
+    {
+        $query = $this->index();
+
+        if ($user->getRole() !== UserRole::ADMIN) {
+            $query
+                ->leftJoin('e.teamMembers', 'tm')
+                ->andWhere('tm.user = :user')
+                ->andWhere('tm.accepted = true')
+                ->setParameter('user', $user)
+            ;
+        }
+
+        return $query;
+    }
+}
