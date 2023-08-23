665d8fd657d983a805547ad45ff6302354234e60
Marcin Fortuniak <fortgeek@gmail.com>
2023-08-23 23:09:06 +0200

    relationship fix

--- src/Entity/Event/Event.php
+++ src/Entity/Event/Event.php
@@ -53,7 +53,7 @@ class Event extends AbstractEntity
     )]
     private string $description;
 
-    #[ORM\OneToOne(targetEntity: File::class)]
+    #[ORM\ManyToOne(targetEntity: File::class)]
     #[ORM\JoinColumn(referencedColumnName: 'uuid', nullable: false)]
     #[Constraints\NotBlank(allowNull: false, groups: [EventGroups::CREATE])]
     #[Groups([
